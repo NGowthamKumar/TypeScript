bd659cc9bc0f6f402c12c96b795bd3e8ecf2144d
N Gowtham Kumar <48080545+NGowthamKumar@users.noreply.github.com>
2020-11-30 19:18:28 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,19 @@
+"use strict";
+exports.__esModule = true;
+var msg = "gowtham kumar";
+console.log(msg);
+var num = 10;
+console.log(num);
+function no(x, y) {
+    return x + y;
+}
+console.log(no(5, 12));
+function str(name) {
+    return "Hi " + name;
+}
+console.log(str("gowtham"));
+function an(x) {
+    return x;
+}
+console.log(an("how are you"));
+console.log(an(56));
